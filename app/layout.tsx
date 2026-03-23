d80bf8f3632905109b6cbab97563389eb3145ab4
Innova Space <emorales@colprovidencia.cl>
2026-03-23 16:57:50 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import SupportButton from "@/components/ui/SupportButton"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -19,7 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" suppressHydrationWarning>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <SupportButton />
+      </body>
     </html>
   )
 }
