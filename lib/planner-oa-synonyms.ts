cea48b7534025decba7f9525c550344ef094524f
Innova Space <emorales@colprovidencia.cl>
2026-06-08 11:29:16 -0400

    feat(planner): add OA topic synonyms

---
+++ lib/planner-oa-synonyms.ts
@@ -0,0 +1,9 @@
+export const OA_SYNONYMS: Record<string, string[]> = {
+  ambiente: ["ambiental", "entorno", "reciclaje", "sustentabilidad"],
+  probabilidad: ["azar", "aleatorio", "evento", "experimento"],
+  energia: ["electricidad", "calor", "solar", "combustion"],
+  reaccion: ["quimica", "oxidacion", "corrosion", "reactantes", "productos"],
+  tecnologia: ["innovacion", "digital", "prototipo", "robotica"],
+  salud: ["bienestar", "autocuidado", "nutricion", "prevencion"],
+  convivencia: ["ciudadania", "respeto", "participacion", "comunidad"],
+}
