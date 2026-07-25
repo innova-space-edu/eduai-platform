80c16b7637127c132795a606eaa6b29659d3e026
Innova Space <emorales@colprovidencia.cl>
2026-07-25 18:32:57 -0400

    feat(creator): habilitar relatos editables

---
+++ app/creator-hub/story/page.tsx
@@ -0,0 +1,5 @@
+import EditableNarrativeCreatorPage from "@/components/creator-hub/EditableNarrativeCreatorPage"
+
+export default function EditableStoryPage() {
+  return <EditableNarrativeCreatorPage format="story" />
+}
