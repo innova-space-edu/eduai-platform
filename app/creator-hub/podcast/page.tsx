0177a1017f0c10e8e594fdf86642ea59366a6514
Innova Space <emorales@colprovidencia.cl>
2026-07-25 18:33:14 -0400

    feat(creator): habilitar guiones de podcast editables

---
+++ app/creator-hub/podcast/page.tsx
@@ -0,0 +1,5 @@
+import EditableNarrativeCreatorPage from "@/components/creator-hub/EditableNarrativeCreatorPage"
+
+export default function EditablePodcastPage() {
+  return <EditableNarrativeCreatorPage format="podcast" />
+}
