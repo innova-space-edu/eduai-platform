ce845108501827422149b890414f107360226387
Innova Space <emorales@colprovidencia.cl>
2026-07-29 16:41:11 -0400

    style: apply giant-display wrapper to creative notebook

--- app/cuaderno-creativo/page.tsx
+++ app/cuaderno-creativo/page.tsx
@@ -1,5 +1,10 @@
 import CreativeNotebook from "./CreativeNotebook"
+import styles from "./display.module.css"
 
 export default function CuadernoCreativoPage() {
-  return <CreativeNotebook />
+  return (
+    <div className={styles.page}>
+      <CreativeNotebook />
+    </div>
+  )
 }
